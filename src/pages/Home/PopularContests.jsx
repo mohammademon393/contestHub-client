69456e01885e2b9e1fb43ab00defc877f0f4e89c
mohammademon393 <mdemon357ep@gmail.com>
2026-05-11 16:17:40 +0600

    Maintaint folder structure  and  create somes jsx components

--- src/pages/Home/PopularContests.jsx
+++ src/pages/Home/PopularContests.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const PopularContests = () => {
     return (
         <div>
-            This is  homeeeee.
+            
         </div>
     );
 };
 
-export default Home;
+export default PopularContests;
---
+++ src/pages/Home/WinnerSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WinnerSection = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default WinnerSection;
