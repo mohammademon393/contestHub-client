d9189d228eb89a13eb8f63c9ae78a5201026184f
mohammademon393 <mdemon357ep@gmail.com>
2026-05-15 21:44:39 +0600

    Implement on the sign out method

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,36 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthContext from "../context/AuthContext";
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../firebase/firebase.init";
 
 const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   // register function
   const registerUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  // sign in function 
-  const signInUser =(email,password) =>{
-    return signInWithEmailAndPassword(auth, email,password)
+  // sign in function
+  const signInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
   };
 
-  // google sign in 
-  const googleSignIn =()=> {
+  // google sign in
+  const googleSignIn = () => {
+    setLoading(true);
     return signInWithPopup(auth, googleProvider);
   };
 
+  // sign out function
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
+
+    // observe auth state change
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+        setLoading(false);
+      });
+
+      return () => unsubscribe();
+    }, []);
 
   // all auth releted info
   const authInfo = {
     registerUser,
-    signInUser, 
-    googleSignIn
+    signInUser,
+    googleSignIn,
+    logOut,
+    user,
+    loading,
   };
-
-
 
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router';
-import Logo from '../components/logo/Logo';
+import React from "react";
+import { Link, NavLink } from "react-router";
+import Logo from "../components/logo/Logo";
 import {
   FaHome,
   FaTrophy,
@@ -9,99 +9,136 @@ import {
   FaSignInAlt,
   FaUserPlus,
 } from "react-icons/fa";
-import { IoCall } from 'react-icons/io5';
+import { IoCall } from "react-icons/io5";
+import useAuth from "../hooks/useAuth";
 
 const Navbar = () => {
+  const {logOut, user} = useAuth();
 
-    const navClass = ({ isActive }) =>
-      isActive
-        ? "text-primary font-medium bg-primary rounded-2xl text-white px-4"
-        : "hover:text-primary hover:underline hover:rounded-2xl";
+  // sign out function 
+  const handleSignOut = () =>{
+    logOut()
+    .then()
+    .catch(error =>{
+      console.error(error);
+    })
+  }
 
-        const links = (
-          <>
-            <li>
-              <NavLink to="/" className={navClass}>
-                <FaHome />
-                Home
-              </NavLink>
-            </li>
+  // function to set active class for nav links
+  const navClass = ({ isActive }) =>
+    isActive
+      ? "text-primary font-medium bg-primary rounded-2xl text-white px-4"
+      : "hover:text-primary hover:underline hover:rounded-2xl";
 
-            <li>
-              <NavLink to="/contests" className={navClass}>
-                <FaTrophy />
-                All Contests
-              </NavLink>
-            </li>
+  // common links for both desktop and mobile
+  const links = (
+    <>
+      <li>
+        <NavLink to="/" className={navClass}>
+          <FaHome />
+          Home
+        </NavLink>
+      </li>
 
-            <li>
-              <NavLink to="/leaderboard" className={navClass}>
-                <FaMedal />
-                Leaderboard
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" className={navClass}>
-                <FaInfoCircle />
-                About Us
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact" className={navClass}>
-                <IoCall />
-                Contact
-              </NavLink>
-            </li>
-          </>
-        );
+      <li>
+        <NavLink to="/contests" className={navClass}>
+          <FaTrophy />
+          All Contests
+        </NavLink>
+      </li>
 
-    return (
-      <div className="navbar bg-base-100 shadow-sm">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
-            </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow gap-1"
+      <li>
+        <NavLink to="/leaderboard" className={navClass}>
+          <FaMedal />
+          Leaderboard
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/about" className={navClass}>
+          <FaInfoCircle />
+          About Us
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact" className={navClass}>
+          <IoCall />
+          Contact
+        </NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+
+      {/* navbar start content */}
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              {links}
-            </ul>
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
           </div>
-          <Link to="/" className="text-2xl font-bold cursor-pointer flex items-center">
-            <Logo></Logo>
-          </Link>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow gap-1"
+          >
+            {links}
+          </ul>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 gap-1">{links}</ul>
-        </div>
-        <div className="navbar-end">
-          <Link to="/register" className="btn btn-primary btn-outline hidden md:inline-flex">
-            <FaUserPlus/>
-            Register
-          </Link>
-          <Link to="/login" className="btn btn-primary ml-2 text-white hover:bg-purple-700">
-            <FaSignInAlt></FaSignInAlt>
-            Login
-          </Link>
-        </div>
+        <Link
+          to="/"
+          className="text-2xl font-bold cursor-pointer flex items-center"
+        >
+          <Logo></Logo>
+        </Link>
       </div>
-    );
+
+      {/* navbar center content */}
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 gap-1">{links}</ul>
+      </div>
+
+      {/* navbar end content */}
+      <div className="navbar-end">
+        {
+          user ? 
+          <button
+          onClick={handleSignOut}
+           className="btn btn-primary hover:bg-purple-700">Sign out</button>
+          : 
+          <div>
+            <Link
+          to="/register"
+          className="btn btn-primary btn-outline hidden md:inline-flex"
+        >
+          <FaUserPlus />
+          Register
+        </Link>
+        <Link
+          to="/login"
+          className="btn btn-primary ml-2 text-white hover:bg-purple-700"
+        >
+          <FaSignInAlt></FaSignInAlt>
+          Login
+        </Link>
+          </div> 
+        }
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
