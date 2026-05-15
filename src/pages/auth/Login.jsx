fb2eba36ea962983e9fc8207038ea17ec88a9834
mohammademon393 <mdemon357ep@gmail.com>
2026-05-15 21:19:50 +0600

    Implement google sign in method

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,61 +1,88 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../hooks/useAuth';
+import SocialLogin from './SocialLogin';
 
 const Login = () => {
+
+      const {register, handleSubmit, formState: { errors }} = useForm();
+      const { signInUser } = useAuth();
+
+      const handleSignIn = (data) => {
+        signInUser(data.email, data.password)
+          .then((result) => {
+            const user = result.user;
+            console.log(user);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      };
+
     return (
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         <div className="card-body">
           <h2 className="text-2xl font-bold">
             Welcome back <span className="text-primary">Contest</span>Hub!
           </h2>
-          <p className="text-sm text-gray-500">Please login to your account.</p>
-          <form>
+          <p className="text-sm text-gray-500">
+            Please login your account.
+          </p>
+          <form onSubmit={handleSubmit(handleSignIn)}>
             <fieldset className="fieldset">
+              
+              {/* email feild */}
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input
+                type="email"
+                {...register("email", { required: true })}
+                className="input"
+                placeholder="Email"
+              />
+              {errors.email && (
+                <span className="text-red-500 text-sm">Email is required</span>
+              )}
+              {/* password feild */}
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
-              <button className="btn btn-neutral mt-4">Login</button>
+              <input
+                type="password"
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  pattern: {
+                    value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/,
+                    message:
+                      "Password must contain uppercase, lowercase and number",
+                  },
+                })}
+                className="input"
+                placeholder="Password"
+              />
+              {errors.password && (
+                <span className="text-red-500 text-sm">
+                  {errors.password.type === "required" &&
+                    "Password is required"}
+                  {errors.password.type === "minLength" &&
+                    "Password must be at least 6 characters"}
+                  {errors.password.type === "pattern" &&
+                    errors.password.message}
+                </span>
+              )}
+
+              <button className="btn btn-neutral mt-4 mr-4">Sign In</button>
             </fieldset>
           </form>
-          <div className="divider -my-1">OR</div>
-          {/* Google */}
-          <button className="btn bg-white text-black border-[#e5e5e5]">
-            <svg
-              aria-label="Google logo"
-              width="16"
-              height="16"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <g>
-                <path d="m0 0H512V512H0" fill="#fff"></path>
-                <path
-                  fill="#34a853"
-                  d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
-                ></path>
-                <path
-                  fill="#4285f4"
-                  d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
-                ></path>
-                <path
-                  fill="#fbbc02"
-                  d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
-                ></path>
-                <path
-                  fill="#ea4335"
-                  d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
-                ></path>
-              </g>
-            </svg>
-            Login with Google
-          </button>
+          <div className="divider -my-0.5 mr-4">OR</div>
+          
+          {/* Social Login (Google login) */}
+                    <SocialLogin></SocialLogin>
 
           <p className="text-center">
-            Don't have an account?{' '}
-            <a href="/register" className="link link-hover font-semibold text-primary">
+            You don't have an account?{" "}
+            <a
+              href="/register"
+              className="link link-hover font-semibold text-primary"
+            >
               Register now!
             </a>
           </p>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import AuthContext from "../context/AuthContext";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase/firebase.init";
+
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
   // register function
@@ -14,11 +16,21 @@ const AuthProvider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email,password)
   };
 
+  // google sign in 
+  const googleSignIn =()=> {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+
+
   // all auth releted info
   const authInfo = {
     registerUser,
     signInUser, 
+    googleSignIn
   };
+
+
 
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
